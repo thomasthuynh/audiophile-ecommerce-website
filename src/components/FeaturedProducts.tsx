0ebb9e44b03d56071e793173e67dcf4035cd85be
ThomasHuynh <Thomas_TTH@live.com>
2023-12-02 15:30:32 -0500

    Completed responsive styling for featured products section

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -68,9 +68,9 @@ const FeaturedProducts = (props: Props) => {
 
   return (
     <main className="py-8">
-      <div className="mx-auto grid w-5/6 gap-12">
+      <div className="mx-auto grid w-5/6 gap-12 md:grid-cols-2">
         {/* ZX9 SPEAKER */}
-        <div className="bg-primary-500 bg-circles overflow-hidden rounded-md bg-[length:525px_525px] bg-[center_top_-100px] bg-no-repeat px-8 py-20 md:bg-[length:775px_775px] md:bg-[center_top_-150px] lg:bg-[left_-175px_top_0px] lg:py-8 xl:bg-[length:925px_925px] min-[1440px]:bg-[length:1025px_1025px] min-[1440px]:bg-[left_-150px_top_0px]">
+        <div className="bg-primary-500 bg-circles overflow-hidden rounded-md bg-[length:525px_525px] bg-[center_top_-100px] bg-no-repeat px-8 py-20 md:col-span-2 md:bg-[length:775px_775px] md:bg-[center_top_-150px] lg:bg-[left_-175px_top_0px] lg:py-8 xl:bg-[length:925px_925px] min-[1440px]:bg-[length:1025px_1025px] min-[1440px]:bg-[left_-150px_top_0px]">
           <div className="mx-auto flex flex-col items-center justify-center lg:flex-row lg:justify-around min-[1440px]:w-[90%]">
             {/* IMAGE */}
             <div className="mx-auto w-3/5 pb-6 sm:w-2/5 lg:translate-y-[20%] xl:w-2/5 xl:translate-y-[15%]">
@@ -98,7 +98,7 @@ const FeaturedProducts = (props: Props) => {
         </div>
 
         {/* ZX7 SPEAKER */}
-        <div className="relative">
+        <div className="relative md:col-span-2">
           <img
             src={getImageSize("speaker")}
             alt="ZX7 Speaker"
@@ -115,14 +115,18 @@ const FeaturedProducts = (props: Props) => {
         </div>
 
         {/* YX1 EARPHONES IMAGE */}
-        <div>
-          <img src={EarphonesSm} alt="YX1 Earphones" className="rounded-md" />
+        <div className="">
+          <img
+            src={getImageSize("earphones")}
+            alt="YX1 Earphones"
+            className="w-full rounded-md"
+          />
         </div>
 
         {/* YX1 EARPHONES */}
-        <div className="rounded-md bg-gray-100 py-20">
-          <div className="pl-6">
-            <h2 className="pb-6 text-2xl uppercase text-black min-[480px]:text-3xl sm:text-4xl">
+        <div className="flex items-center rounded-md bg-gray-100">
+          <div className="p-6 sm:p-10 lg:px-16 min-[1440px]:px-24">
+            <h2 className="pb-4 text-2xl uppercase text-black min-[480px]:text-3xl sm:pb-6 sm:text-4xl xl:pb-8 xl:text-5xl">
               YX1 Earphones
             </h2>
             <button className="border-[1px] border-black bg-transparent px-4 py-2 text-sm uppercase tracking-[1px] text-black hover:bg-black hover:text-white sm:px-8 sm:py-4 sm:text-base">
