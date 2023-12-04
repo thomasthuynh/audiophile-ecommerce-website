5d43e74fc76cffbd89cf5767e1be38a49d073808
ThomasHuynh <Thomas_TTH@live.com>
2023-12-03 23:52:56 -0500

    Adjusted company info text and image sizes for larger screens

--- src/components/CompanyInfo.tsx
+++ src/components/CompanyInfo.tsx
@@ -7,30 +7,36 @@ type Props = {};
 const CompanyInfo = (props: Props) => {
   return (
     <section className="py-24">
-      <div className="mx-auto grid w-5/6 gap-8 xl:grid-cols-2">
+      <div className="mx-auto w-5/6 grid gap-8 xl:grid-cols-2">
+ 
+          <div className="overflow-hidden rounded-md xl:order-2 2xl:w-4/5 2xl:mx-auto">
+            <picture>
+              <source media="(min-width: 1280px)" srcSet={BestGearLg} />
+              <source media="(min-width: 768px)" srcSet={BestGearMd} />
+              <img
+                src={BestGearSm}
+                alt="Man wearing headphones"
+                className="w-full"
+              />
+            </picture>
+          </div>
 
-        <div className="overflow-hidden rounded-md xl:order-2 max-w-[580px]">
-          <picture>
-            <source media="(min-width: 1280px)" srcSet={BestGearLg}/>
-            <source media="(min-width: 768px)" srcSet={BestGearMd} />
-            <img src={BestGearSm} alt="Man wearing headphones" className="w-full"/>
-          </picture>
-        </div>
+          <div className="flex-col justify-center text-center xl:flex xl:w-4/5  xl:text-left 2xl:mx-auto">
+            <h3 className="py-8 text-3xl uppercase xl:text-5xl">
+              Bringing you the <span className="text-primary-500">best</span>{" "}
+              audio gear
+            </h3>
+            <p className="text-neutral-700 xl:text-lg">
+              Located at the heart of New York City, Audiophile is the premier
+              store for high end headphones, earphones, speakers, and audio
+              accessories. We have a large showroom and luxury demonstration
+              rooms available for you to browse and experience a wide range of
+              our products. Stop by our store to meet some of the fantastic
+              people who make Audiophile the best place to buy your portable
+              audio equipment.
+            </p>
+          </div>
 
-        <div className="flex-col justify-center text-center xl:flex xl:text-left  xl:w-4/5">
-          <h3 className="py-8 text-3xl uppercase xl:text-5xl">
-            Bringing you the <span className="text-primary-500">best</span>{" "}
-            audio gear
-          </h3>
-          <p className="text-neutral-700 xl:text-lg">
-            Located at the heart of New York City, Audiophile is the premier
-            store for high end headphones, earphones, speakers, and audio
-            accessories. We have a large showroom and luxury demonstration rooms
-            available for you to browse and experience a wide range of our
-            products. Stop by our store to meet some of the fantastic people who
-            make Audiophile the best place to buy your portable audio equipment.
-          </p>
-        </div>
       </div>
     </section>
   );
