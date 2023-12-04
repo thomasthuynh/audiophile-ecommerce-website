08a4f62b08e0651fa6da134120f94b6772eda6c9
ThomasHuynh <Thomas_TTH@live.com>
2023-12-03 23:33:15 -0500

    Styled responve company info section. Testing breakpoint adjustments for featured products section

--- src/components/CompanyInfo.tsx
+++ src/components/CompanyInfo.tsx
@@ -1,13 +1,39 @@
+import BestGearSm from "../assets/shared/mobile/image-best-gear.jpg";
+import BestGearMd from "../assets/shared/tablet/image-best-gear.jpg";
+import BestGearLg from "../assets/shared/desktop/image-best-gear.jpg";
 
-
-type Props = {}
+type Props = {};
 
 const CompanyInfo = (props: Props) => {
   return (
-    <section className="py-8">
-      <div className="w-5/6"></div>
-    </section>
-  )
-}
+    <section className="py-24">
+      <div className="mx-auto grid w-5/6 gap-8 xl:grid-cols-2">
 
-export default CompanyInfo
+        <div className="overflow-hidden rounded-md xl:order-2 max-w-[580px]">
+          <picture>
+            <source media="(min-width: 1280px)" srcSet={BestGearLg}/>
+            <source media="(min-width: 768px)" srcSet={BestGearMd} />
+            <img src={BestGearSm} alt="Man wearing headphones" className="w-full"/>
+          </picture>
+        </div>
+
+        <div className="flex-col justify-center text-center xl:flex xl:text-left  xl:w-4/5">
+          <h3 className="py-8 text-3xl uppercase xl:text-5xl">
+            Bringing you the <span className="text-primary-500">best</span>{" "}
+            audio gear
+          </h3>
+          <p className="text-neutral-700 xl:text-lg">
+            Located at the heart of New York City, Audiophile is the premier
+            store for high end headphones, earphones, speakers, and audio
+            accessories. We have a large showroom and luxury demonstration rooms
+            available for you to browse and experience a wide range of our
+            products. Stop by our store to meet some of the fantastic people who
+            make Audiophile the best place to buy your portable audio equipment.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CompanyInfo;
--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -45,9 +45,9 @@ const FeaturedProducts = (props: Props) => {
   const getImageSize = (product: ProductKey): string | undefined => {
     if (viewWidth < 768) {
       return imageSource[product].small;
-    } else if (viewWidth >= 768 && viewWidth < 1024) {
+    } else if (viewWidth >= 768 && viewWidth < 1280) {
       return imageSource[product].medium;
-    } else if (viewWidth >= 1024) {
+    } else if (viewWidth >= 1280) {
       return imageSource[product].large;
     } else {
       return undefined;
@@ -70,19 +70,19 @@ const FeaturedProducts = (props: Props) => {
     <main className="py-8">
       <div className="mx-auto grid w-5/6 gap-12 md:grid-cols-2">
         {/* ZX9 SPEAKER */}
-        <div className="bg-primary-500 bg-circles overflow-hidden rounded-md bg-[length:525px_525px] bg-[center_top_-100px] bg-no-repeat px-8 py-20 md:col-span-2 md:bg-[length:775px_775px] md:bg-[center_top_-150px] lg:bg-[left_-175px_top_0px] lg:py-8 xl:bg-[length:925px_925px] min-[1440px]:bg-[length:1025px_1025px] min-[1440px]:bg-[left_-150px_top_0px]">
-          <div className="mx-auto flex flex-col items-center justify-center lg:flex-row lg:justify-around min-[1440px]:w-[90%]">
+        <div className="bg-primary-500 bg-circles overflow-hidden rounded-md bg-[length:525px_525px] bg-[center_top_-100px] bg-no-repeat px-8 py-20 md:col-span-2 md:bg-[length:775px_775px] md:bg-[center_top_-150px] xl:bg-[left_-175px_top_0px] xl:py-8 xl:bg-[length:925px_925px] min-[1440px]:bg-[length:1025px_1025px] min-[1440px]:bg-[left_-150px_top_0px]">
+          <div className="mx-auto flex flex-col items-center justify-center xl:flex-row xl:justify-around min-[1440px]:w-[90%]">
             {/* IMAGE */}
-            <div className="mx-auto w-3/5 pb-6 sm:w-2/5 lg:translate-y-[20%] xl:w-2/5 xl:translate-y-[15%]">
+            <div className="mx-auto w-3/5 pb-6 sm:w-2/5 xl:translate-y-[20%] xl:w-2/5 xl:translate-y-[15%]">
               <img
                 src={getImageSize("primarySpeaker")}
                 alt="ZX9 Speaker"
-                className="lg:max-w-[285px] xl:max-w-[400px]"
+                className="xl:max-w-[285px] xl:max-w-[400px]"
               />
             </div>
 
             {/* TEXT AND BUTTON*/}
-            <div className="text-center lg:w-1/2 lg:items-start lg:px-4 lg:text-left xl:mx-auto xl:w-2/5">
+            <div className="text-center xl:w-1/2 xl:items-start xl:px-4 xl:text-left xl:mx-auto xl:w-2/5">
               <h2 className="py-6 text-4xl uppercase text-white sm:text-5xl">
                 ZX9 Speaker
               </h2>
@@ -90,7 +90,7 @@ const FeaturedProducts = (props: Props) => {
                 Upgrade to premium speakers that are phenomenally built to
                 deliver truly remarkable sound.
               </p>
-              <button className="transparent bg-black px-6 py-3 text-sm uppercase tracking-[1px] text-white hover:bg-neutral-700 sm:px-8 sm:py-4 sm:text-base lg:px-6 lg:py-3">
+              <button className="transparent bg-black px-6 py-3 text-sm uppercase tracking-[1px] text-white hover:bg-neutral-700 sm:px-8 sm:py-4 sm:text-base xl:px-6 xl:py-3">
                 See Product
               </button>
             </div>
