ea7f58c41c4fb3bb731863f37db272f24673cd8a
ThomasHuynh <Thomas_TTH@live.com>
2023-12-16 23:22:21 -0500

    Completed ProductInfo responsive styling for desktop design

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -19,6 +19,7 @@ const ProductInfo = ({ productData }: Props) => {
   return (
     <div className="pt-[74px]">
       <div className="mx-auto w-5/6">
+        {/* LINK */}
         <Link
           to={`/${product.category}`}
           className="mb-4 mt-6 inline-flex items-center capitalize text-neutral-500"
@@ -27,9 +28,9 @@ const ProductInfo = ({ productData }: Props) => {
           <p>{product.category}</p>
         </Link>
         {/* PRODUCT */}
-        <div className="flex flex-col md:flex-row md:gap-8">
+        <div className="flex flex-col md:flex-row md:gap-8 lg:gap-12 xl:justify-between">
           {/*IMAGE */}
-          <div className="py-4 md:w-[45%] md:max-w-[400px] md:pr-4 lg:pr-8">
+          <div className="py-4 md:w-[45%] md:max-w-[400px] md:pr-4 xl:max-w-[750px]">
             <picture>
               <source
                 media="(min-width: 1280px)"
@@ -48,57 +49,61 @@ const ProductInfo = ({ productData }: Props) => {
           </div>
 
           {/* INFO */}
-          <div className="flex flex-col md:flex-1 md:justify-center">
-            <div>
-              {product.new && (
-                <p className="py-2 text-xs uppercase tracking-[10px] text-primary-500 sm:text-base">
-                  New Product
+          <div className="flex max-w-[850px] flex-col md:flex-1 md:justify-center xl:w-1/2 xl:flex-none">
+            <div className="xl:w-[90%] mx-auto 2xl:w-5/6">
+              <div>
+                {product.new && (
+                  <p className="py-2 text-xs uppercase tracking-[10px] text-primary-500 sm:text-base">
+                    New Product
+                  </p>
+                )}
+
+                <h2 className="pt-4 text-2xl uppercase sm:text-3xl xl:text-4xl">
+                  {product.name}
+                </h2>
+
+                <p className="py-6 text-neutral-500 xl:text-lg">
+                  {product.description}
                 </p>
-              )}
+                <p className="text-lg font-bold tracking-[1px] md:text-xl">
+                  ${product.price}
+                </p>
+              </div>
 
-              <h2 className="pt-4 text-2xl uppercase sm:text-3xl xl:text-4xl">
-                {product.name}
-              </h2>
-
-              <p className="py-6 text-neutral-500">
-                {product.description}
-              </p>
-              <p className="text-lg font-bold tracking-[1px] md:text-xl">
-                ${product.price}
-              </p>
-            </div>
-
-            <div className="flex gap-4 py-6">
-              <input
-                type="number"
-                className="w-1/3 max-w-[115px] bg-gray-100 p-4 font-bold"
-              />
-              <button className="bg-primary-500 px-6 py-3 text-sm uppercase tracking-[1px] text-white hover:bg-primary-300 sm:px-8 sm:py-4 sm:text-base">
-                Add to Cart
-              </button>
+              <div className="flex gap-4 py-6">
+                <input
+                  type="number"
+                  className="w-1/3 max-w-[115px] bg-gray-100 p-4 font-bold"
+                />
+                <button className="bg-primary-500 px-6 py-3 text-sm uppercase tracking-[1px] text-white hover:bg-primary-300 sm:px-8 sm:py-4 sm:text-base">
+                  Add to Cart
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
-        {/* FEATURES  */}
-        <div className="py-16">
-          <h3 className="py-4 text-xl uppercase md:text-2xl">Features</h3>
-          <p className="text-neutral-500">{product.features}</p>
-        </div>
+        <div className="grid py-16 xl:grid-cols-6 xl:justify-between">
+          {/* FEATURES  */}
+          <div className="xl:col-span-3">
+            <h3 className="py-4 text-xl uppercase md:text-2xl">Features</h3>
+            <p className="text-neutral-500 xl:text-lg">{product.features}</p>
+          </div>
 
-        {/* IN THE BOX */}
-        <div>
-          <h3 className="py-4 text-xl uppercase md:text-2xl">In The Box</h3>
-          <ul>
-            {product.includes.map((addon, index) => (
-              <li className="py-1 text-neutral-500" key={index}>
-                <span className="pr-4 font-bold text-primary-500">
-                  {addon.quantity}x
-                </span>
-                {addon.item}
-              </li>
-            ))}
-          </ul>
+          {/* IN THE BOX */}
+          <div className="xl:col-span-2 xl:col-start-5">
+            <h3 className="py-4 text-xl uppercase md:text-2xl">In The Box</h3>
+            <ul className="xl:text-lg">
+              {product.includes.map((addon, index) => (
+                <li className="py-1 text-neutral-500" key={index}>
+                  <span className="pr-4 font-bold text-primary-500">
+                    {addon.quantity}x
+                  </span>
+                  {addon.item}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
         {/* GALLERY */}
