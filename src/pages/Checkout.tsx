dbf50664ba6348dfe99801d184551ab4cb512a55
ThomasHuynh <Thomas_TTH@live.com>
2023-12-22 21:43:42 -0500

    Removed unused props from Checkout component

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import { useContext } from "react";
+import CartContext from "../context/CartContext";
 
-type Props = {};
+const Checkout = () => {
+  const {
+    state: { cart },
+  } = useContext(CartContext);
 
-const Checkout = (props: Props) => {
+  const subtotal = cart.reduce((items, currentItem) => items + currentItem.price * (currentItem.quantity ?? 0), 0)
+
   return (
-    <div className="bg-gray-100 pt-[108px]">
+    <div className="bg-gray-20 pt-[108px]">
       {/* FORM */}
-      <div className="mx-auto w-5/6 rounded-lg bg-white p-6">
+      <div className="mx-auto mb-8 w-5/6 rounded-lg bg-white p-6 shadow-md">
         <h1 className="pb-4 text-xl uppercase">Checkout</h1>
 
         <form action="submit">
@@ -16,7 +21,7 @@ const Checkout = (props: Props) => {
               Billing Details
             </legend>
 
-            <label htmlFor="name" className="pb-2 text-xs font-bold inline">
+            <label htmlFor="name" className="inline pb-2 text-xs font-bold">
               Name
             </label>
             <input
@@ -124,6 +129,71 @@ const Checkout = (props: Props) => {
             />
           </fieldset>
         </form>
+      </div>
+
+      {/* SUMMARY */}
+      <div className="mx-auto mb-8 w-5/6 rounded-lg bg-white p-6 shadow-md">
+        <h2 className="pb-4 text-xl uppercase">Summary</h2>
+        <div>
+          {cart.map((product) => (
+            <div
+              key={product.id}
+              className="flex w-full items-center justify-between py-3"
+            >
+              <div className="flex items-center">
+                <div className="max-w-[60px]">
+                  <img
+                    src={product.image.mobile}
+                    alt={product.name}
+                    className="w-full rounded-md"
+                  />
+                </div>
+
+                <div className="flex flex-col pl-4 font-bold">
+                  <p className="text-sm">{product.name.split(" ")[0]}</p>
+                  <p className="text-xs text-neutral-500">${product.price}</p>
+                </div>
+              </div>
+
+              <div>
+                <p className="font-bold text-neutral-500">
+                  x{product.quantity}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* TOTAL */}
+        <div className="flex flex-col gap-2 py-4">
+          {/* SUBTOTAL */}
+          <div className="flex justify-between items-center">
+            <p className="uppercase text-neutral-500 text-sm">Total</p>
+            <p className="font-bold">${subtotal.toFixed(2)}</p>
+          </div>
+
+          {/* SHIPPING */}
+          <div className="flex justify-between items-center">
+            <p className="uppercase text-neutral-500 text-sm">SHIPPING (2.5%)</p>
+            <p className="font-bold">${(subtotal * 0.025).toFixed(2)}</p>
+          </div>
+
+          {/* TAX */}
+          <div className="flex justify-between items-center">
+            <p className="uppercase text-neutral-500 text-sm">TAX (10%)</p>
+            <p className="font-bold">${(subtotal * 0.1).toFixed(2)}</p>
+          </div>
+
+          {/* GRAND TOTAL */}
+          <div className="flex justify-between items-center py-4">
+            <p className="uppercase text-neutral-500 text-sm">GRAND TOTAL</p>
+            <p className="font-bold text-primary-500">${(subtotal * 1.125).toFixed(2)}</p>
+          </div>
+
+          <button className="mt-2 inline-block w-full bg-primary-500 px-6 py-3 text-center text-sm uppercase tracking-[1px] text-white hover:bg-primary-300 sm:px-8 sm:py-4 sm:text-base">
+            Continue and Pay
+          </button>
+        </div>
       </div>
     </div>
   );
